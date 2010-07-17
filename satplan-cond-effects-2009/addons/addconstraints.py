47966ed08c542839fb73c0fce77b055a2e705234
Jose <jose@volker.(none)>
2010-07-17 16:44:58 -0430

    listo sometimes-before

--- satplan-cond-effects-2009/addons/addconstraints.py
+++ satplan-cond-effects-2009/addons/addconstraints.py
@@ -2,6 +2,14 @@ import sys
 import os
 import re
 import itertools
+import math
+
+def is_cons(a):
+    cons = ["always","sometimes","at end","at-most-once","sometimes-after","sometimes-before"]
+    for i in range(0,len(cons)):
+        if a==cons[i]:
+            return i
+    return -1
 
 CNF_nf=sys.argv[1]
 action_table_nf=sys.argv[2]
@@ -27,98 +35,163 @@ while not line_ok:
 # Se obtienen los constraints del archivo
 #########################################
 
-# Aqui deberia ir la llamada a la funcion o algo a la parte de marion
-
-# 0 always
-# 1 sometimes
-# 2 at end
-# 3 at-most-once
-# 4 sometimes-after
-# 5 sometimes-before
-codcons = 2
-
-varcons = "ON D A"
-
-########################################
-# Se obtienen nuevas clausulas
-########################################
-
-at=open(action_table_nf,'r')
-temp = []
-for l in at:
-    s = l.split()
-    if (s[0]=="fluent"):
-        if (re.search(varcons,l)):
-            ls1 = l.split(":")
-            ls2 = ls1[1].split("(")
-            numvar = ls2[0]
-            numvar = numvar.lstrip()
-            numvar = numvar.rstrip()
-            temp.append(numvar)
-at.close()
-
 ########################################
 # Se guarda el nuevo CNF
 ########################################
 
 cnf_new_f=open(CNF_new_nf,'w+')
 cnf_new_f.write(first_line)
+cnf_new_f.write("p cnf %d %d\n" % (orig_vars, orig_clauses*orig_clauses) )
 
-if (codcons==3):
-    orig_clauses = orig_clauses+(len(temp))
+# Aqui deberia ir la llamada a la funcion o algo a la parte de marion
 
-cnf_new_f.write("p cnf %d %d\n" % (orig_vars, orig_clauses) )
+result = ["sometimes-before","ON C D","CLEAR A"]
 
-# Se imprimen las clausulas viejas
-for l in CNF:
-    cnf_new_f.write(l)
+########################################
+# Se obtienen nuevas clausulas
+########################################
 
-# Las nuevas se imprimen en otro archivo para mantenerlas
+for cont in range(0,len(result)):
+    codcons = is_cons(result[cont])
+    if codcons != -1:
+        # 0 always
+        # 1 sometimes
+        # 2 at end
+        # 3 at-most-once
+        # 4 sometimes-after
+        # 5 sometimes-before
+        
+        varcons = result[cont]
 
-# Caso sometimes
-if (codcons == 1):
-    for lit in temp:
-        faux.write(str(lit)+' ')
-    faux.write('0\n')
+        temp = []
+        temp1 = []
+        temp2 = []
 
-# Caso always
-if (codcons == 0):
-    for lit in temp:
-        faux.write(str(lit)+' 0\n')
+        at=open(action_table_nf,'r')
+        # El caso que sea sometimes-after
+        # o sometimes-before
+        if ( (is_cons(varcons)==4) | (is_cons(varcons)==5)):
+            varcons1 = result[cont+1]
+            varcons2 = result[cont+2]
+            for l in at:
+                s = l.split()
+                if (s[0]=="fluent"):
+                    if (re.search(varcons1,l)):
+                        print l
+                        ls1 = l.split(":")
+                        lstime = ls1[0].split(" ")
+                        itime = int(lstime[1])
+                        ls2 = ls1[1].split("(")
+                        numvar = ls2[0]
+                        numvar = numvar.lstrip()
+                        numvar = numvar.rstrip()
+                        temp1.append([numvar,itime])
+                    if (re.search(varcons2,l)):
+                        print l
+                        ls1 = l.split(":")
+                        lstime = ls1[0].split(" ")
+                        itime = int(lstime[1])
+                        ls2 = ls1[1].split("(")
+                        numvar = ls2[0]
+                        numvar = numvar.lstrip()
+                        numvar = numvar.rstrip()
+                        temp2.append([numvar,itime])
+            cont = cont + 3
+        # El caso que sea always, sometimes,
+        # at-end o at-most-once
+        else:
+            varcons1 = result[cont+1]
+            for l in at:
+                s = l.split()
+                if (s[0]=="fluent"):
+                    if (re.search(varcons1,l)):
+                        ls1 = l.split(":")
+                        ls2 = ls1[1].split("(")
+                        numvar = ls2[0]
+                        numvar = numvar.lstrip()
+                        numvar = numvar.rstrip()
+                        temp.append(numvar)
+            cont=cont+2
+        at.close()
 
-# Caso at end
-if (codcons == 2):
-    print '\n\n**********\n\n'
-    cnf_new_f.write(temp[-1]+' 0\n')
-    print '\n\n**********\n\n'
+        # Se imprimen las clausulas viejas
+        # y las nuevas se imprimen en otro 
+        # archivo para mantenerlas
+        for l in CNF:
+            cnf_new_f.write(l)
 
-# Caso at-most-once
-if (codcons == 3):
-    print '\n\n**********\n\n'
+########################################
+# Constraint segun el caso
+########################################
 
-    c = []
-    for i in range(0,len(temp)):
-        t = []
-        for j in range(0,len(temp)):
-            if i==j:
-                t.append(str(temp[j]))
-            else:
-                t.append('-'+str(temp[j]))
-        c.append(t)
-#    print c
+        # Caso always
+        if (codcons == 0):
+            for lit in temp:
+                faux.write(str(lit)+' 0\n')
 
-    l = []
-    for i in range(0,len(temp)):
-        l.append(i)
-    a = list(itertools.product(l,repeat=len(temp)))
-    for i in a:
-        for j in range(0,len(temp)):
-#            print c[j][i[j]],
-            faux.write(c[j][i[j]]+' ')
-        faux.write(' 0\n')
-#        print
-                        
-    print '\n\n**********\n\n'
+        # Caso sometimes
+        elif (codcons == 1):
+            for lit in temp:
+                faux.write(str(lit)+' ')
+            faux.write('0\n')
+
+        # Caso at end
+        elif (codcons == 2):
+            cnf_new_f.write(temp[-1]+' 0\n')
+
+        # Caso at-most-once
+        elif (codcons == 3):
+            print '\n\n**********\n\n'
+
+            c = []
+            for i in range(0,len(temp)):
+                t = []
+                for j in range(0,len(temp)):
+                    if i==j:
+                        t.append(str(temp[j]))
+                    else:
+                        t.append('-'+str(temp[j]))
+                c.append(t)
+
+            lo = c
+            lt = c
+
+            for i in range(0,len(temp)):
+                lt = itertools.product(lo,lt)
+                lt.replace("(","")
+                lt.replace(")","")
+
+            for p in lt:                
+                print p
+
+            print '\n\n**********\n\n'
+
+
+        # Caso sometimes-after
+        elif (codcons == 4):
+            print '\n\n**********\n\n'
+            print temp1
+            print temp2
+            print '\n\n**********\n\n'
+
+        # Caso sometimes-before
+        elif (codcons == 5):
+            print '\n\n**********\n\n'
+
+            faux.write('-'+str(temp2[-1][0])+' ')
+            print '-',temp2[-1][0],
+            limit = temp2[-1][1]
+            maxvar = min(limit,len(temp1))
+            for it1 in range(0,maxvar):
+                if (temp1[it1][1]<temp2[-1][1]):
+                    faux.write(str(temp1[it1][0])+' ')
+                    print temp1[it1][0],
+            print
+            faux.write('0\n')
+                            
+            print temp1
+            print temp2
+            print '\n\n**********\n\n'
 
 # Imprimo las clausulas acumuladas en el archivo auxiliar
 # en el nuevo CNF
@@ -127,7 +200,6 @@ faux.close()
 faux = open("auxiliar.txt","r")
 
 for i in faux:
-    print i
     cnf_new_f.write(i)
 
 faux.close()
